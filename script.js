9d921991d068aa2a1bc8292c0017491d6afac816
SREYANSH BARANWAL <sreyanshbaranwal@gmail.com>
2024-02-28 20:31:01 +0530

    added logo

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
+    const target = document.querySelector(tab.dataset.tabTarget);
+
+    // Logic for tab content visibility
     tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
+      tabContent.classList.remove('active');  
+    });
+    target.classList.add('active'); 
+
+    // Logic for tab logos and active class
     tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
+      tab.classList.remove('active'); 
+      tab.querySelector('.tab-logo').style.display = 'none'; // Hide logos
+    });
+    tab.classList.add('active'); 
+    tab.querySelector('.tab-logo').style.display = 'block'; // Show logo for active
+  });
+});
